5e8714e230d37afb1c12be66920054f1a6159567
perplexedcaleb <c.howard14@students.clark.edu>
2020-05-13 12:15:05 -0700

    Completed Code

---
+++ Mod4Lab2.py
@@ -0,0 +1,26 @@
+"""
+CTEC 121
+Caleb Howard
+<assignment/lab name>
+<assignment/lab description
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+from graphics import *
+
+def main():
+    win = GraphWin("demo", 800, 800)
+    win.setCoords(-4.0, -4.0, 4.0, 4.0)
+    p1 = Circle(Point(2, 3), 0.1)
+    p1.setFill("red")
+    p1.draw(win)
+    p2 = Point(-3, 1).draw(win)
+    p3 = Point(-1.5, -2.5).draw(win)
+
+    input()
+
+main()    
